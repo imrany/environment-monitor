e22f622ae443ae4953900c6cb8b16cfb75987370
imrany <imranmat254@gmail.com>
2024-11-07 02:19:50 +0300

    modified to client's request

--- server/app.py
+++ server/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import mysql.connector
+from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
+# allow cors
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})
 
 # Connect to MySQL
 db_connection = mysql.connector.connect(
     host="localhost",
-    user="your_mysql_user",
-    password="your_mysql_password",
+    user="root", #your_mysql_user
+    password="12345678", #your_mysql_password
     database="sensor_data_db"
 )
 cursor = db_connection.cursor()
@@ -30,50 +33,52 @@ CREATE TABLE IF NOT EXISTS sensor_data (
 ''')
 db_connection.commit()
 
+# Template routes
+@app.route('/',methods=['GET'])
+def index_page():
+    # Prepare SQL query for retrieving data
+    query = 'SELECT * FROM sensor_data ORDER BY timestamp DESC'
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    sensory_data = []
+    for row in results:
+        sensory_data.append({
+            "id":row[0],
+            "timestamp": row[1],
+            "temperature": row[2],
+            "humidity": row[3],
+            "heat_index": row[4], # heatIndex
+            "noise": row[5],
+            "CO2": row[6], # mq2_ppm
+            "methane": row[7], # mq2_ppm
+            "SO2": row[8], # mq135_ppm
+            "dust": row[9] # dustDensityStr
+        })
+    return render_template('index.html',sensory_data=sensory_data)
+
 # Endpoint to receive data
-@app.route('/api/sensor_data', methods=['POST','GET'])
+@app.route('/api/sensor_data', methods=['POST'])
 def receive_sensor_data():
-    if request.method == 'POST':
-        data = request.json
-        timestamp = datetime.now()
+    data = request.json
+    timestamp = datetime.now()
 
-        # Prepare SQL query
-        query = '''
-        INSERT INTO sensor_data (timestamp, temperature, humidity, heat_index, noise, CO2, methane, SO2, dust)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        '''
-        values = (
-            timestamp, data['temperature'], data['humidity'], data['heatIndex'],
-            data['noise'], data['mq2_ppm'], data['mq3_ppm'], data['mq135_ppm'], data['dustDensityStr']
-        )
+    # Prepare SQL query
+    query = '''
+    INSERT INTO sensor_data (timestamp, temperature, humidity, heat_index, noise, CO2, methane, SO2, dust)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    '''
+    values = (
+        timestamp, data['temperature'], data['humidity'], data['heatIndex'],
+        data['noise'], data['mq2_ppm'], data['mq3_ppm'], data['mq135_ppm'], data['dustDensityStr']
+    )
 
-        cursor.execute(query, values)
-        db_connection.commit()
-
-        return jsonify({"message": "Data received successfully"}), 200
-    elif request.method == 'GET':
-        # Prepare SQL query for retrieving data
-        query = 'SELECT * FROM sensor_data ORDER BY timestamp DESC'
-        cursor.execute(query)
-        results = cursor.fetchall()
-
-        # Format the results for JSON response
-        data = []
-        for row in results:
-            data.append({
-                "timestamp": row[0],
-                "temperature": row[1],
-                "humidity": row[2],
-                "heat_index": row[3],
-                "noise": row[4],
-                "CO2": row[5],
-                "methane": row[6],
-                "SO2": row[7],
-                "dust": row[8]
-            })
-        return jsonify(data), 200
+    cursor.execute(query, values)
+    db_connection.commit()
+    return jsonify({"message": "Data received successfully"}), 200
 
 # Run the server
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
 
